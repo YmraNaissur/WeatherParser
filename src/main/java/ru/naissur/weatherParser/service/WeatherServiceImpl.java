ea16334655079d611bc231163459f7c8d387b2ae
YmraNaissur <daily.spb@yandex.ru>
2017-04-27 18:47:45 +0300

    Порефакторил

--- src/main/java/ru/naissur/weatherParser/service/WeatherServiceImpl.java
+++ src/main/java/ru/naissur/weatherParser/service/WeatherServiceImpl.java
@@ -14,11 +14,21 @@ import java.util.List;
  * 24.04.2017
  */
 public class WeatherServiceImpl implements WeatherService {
-    @Override
-    public List<String> getStringDates(Elements dateElements) {
-        return ParserUtil.getStringDates(dateElements);
+    private Document document; // разобранная страница
+    private Elements weatherTables; // вытащенные из html таблицы с погодой каждого дня
+
+    // инициализируем document и weatherTables
+    public WeatherServiceImpl() {
+        document = getWeatherInfo();
+        weatherTables = ParserUtil.getWeaterTables(document);
     }
 
+    @Override
+    public List<String> getStringDates(Document document) {
+        return ParserUtil.getStringDates(document);
+    }
+
+    @Override
     public List<String> getData(Element weatherTable, String selector) {
         return ParserUtil.getData(weatherTable, selector);
     }
@@ -29,18 +39,27 @@ public class WeatherServiceImpl implements WeatherService {
     }
 
     @Override
+    public List<String> getRealTemps(Element weatherTable) {
+        return ParserUtil.getRealTemps(weatherTable);
+    }
+
+    @Override
+    public List<String> getFeelTemps(Element weatherTable) {
+        return ParserUtil.getFeelTemps(weatherTable);
+    }
+
+    @Override
     public List<WeatherBean> getWeatherBeans() {
         List<WeatherBean> weatherBeans = new ArrayList<>();
 
-        Document document = getWeatherInfo();
-        Elements weatherTables = ParserUtil.getWeaterTables(document);
-        List<String> dates = getStringDates(document.select("strong[class=forecast-detailed__day-number]"));
+        List<String> dates = getStringDates(document); // список дат
 
         for (int i = 0; i < weatherTables.size(); i++) {
             Element weatherTable = weatherTables.get(i);
-            String[] names = weatherTable.select("div[class=weather-table__daypart").text().split(" ");
-            String[] temps = weatherTable.select("div[class=weather-table__temp]").text().split(" [+−]?\\d\\s?");
 
+            List<String> realTemps = getRealTemps(weatherTable); // реальная температура
+            List<String> feelTemps = getFeelTemps(weatherTable); // ощущаемая температура
+            List<String> names = getData(weatherTable, ParserUtil.DAYPART_NAMES); // названия частей дня
             List<String> events = getData(weatherTable, ParserUtil.EVENTS); // погодные явления дня
             List<String> pressures = getData(weatherTable, ParserUtil.PRESSURES); // значения давлений дня
             List<String> humidities = getData(weatherTable, ParserUtil.HUMIDITIES); // значения влажности
@@ -49,8 +68,8 @@ public class WeatherServiceImpl implements WeatherService {
             Elements windSpeeds = weatherTable.select("span[class=wind-speed]");
             Daypart[] beanDayparts = new Daypart[4];
             for (int j = 0; j < 4; j++) {
-                beanDayparts[j] = new Daypart(names[j], temps[j], events.get(j), pressures.get(j), humidities.get(j),
-                        windsDirections.remove(0).text() + " " + windSpeeds.remove(0).text());
+                beanDayparts[j] = new Daypart(names.get(j), realTemps.get(j), events.get(j), pressures.get(j), humidities.get(j),
+                        windsDirections.remove(0).text() + " " + windSpeeds.remove(0).text(), feelTemps.get(j));
             }
 
             weatherBeans.add(new WeatherBean(dates.get(i), beanDayparts));
